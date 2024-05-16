2d669a539c043536ae35a82e952d3915a37e75a0
Josue Rosario <josue.rosario@escolar509.sp.senai.br>
2024-05-15 22:07:57 -0300

    script para criar o banco

--- models/banco.js
+++ models/banco.js
@@ -9,3 +9,18 @@ const pool = mariadb.createPool({
 
 module.exports = pool
 
+
+// ====================================================================
+// SCRIPT PARA CRIAR O BANCO
+// ====================================================================
+
+// create database nodedb;
+
+// use nodedb;
+
+// CREATE TABLE teste (campo1 varchar(255))
+
+// insert into teste(campo1) values('dbdado1');
+// insert into teste(campo1) values('dbdado2');
+// insert into teste(campo1) values('dbdado3');
+// insert into teste(campo1) values('dbdado4');
